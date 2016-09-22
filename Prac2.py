58fe260d639670c029bdef6cf259e94bfddec77d
TedMichaelDel <tonyalmeida24@qq.com>
2016-09-22 20:32:38 +0800

    do this change

--- Prac2.py
+++ Prac2.py
@@ -1,5 +1,12 @@
 a = input()
 b = input()
-def liangshuzhihe(x,y):
-	return x + y
-print liangshuzhihe(a,b)
+
+
+def liangshuzhihe(x, y):
+    return x + y
+
+
+print liangshuzhihe(a, b)
+
+print '++__++__++__+'
+print "This one is for you and me"
---
+++ Prac4.py
@@ -0,0 +1,5 @@
+he = 0
+l = range(1,101)
+for i in l:
+	he = he + i
+print he
